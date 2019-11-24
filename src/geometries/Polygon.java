ffa61a0f632dd29a101bdba38dcd85d446c1829f
asafJct <44846049+asafJct@users.noreply.github.com>
2019-11-24 20:28:48 +0200

    Update Polygon.java

--- src/geometries/Polygon.java
+++ src/geometries/Polygon.java
@@ -8,9 +8,9 @@ public class Polygon implements Geometry {
 	List<Point3D> _points;
 	Plane _plane;
 
-	public Polygon(List<Point3D> _points) {
-		super();
+	public Polygon(List<Point3D> _points , plane) {
 		this._points = _points;
+		_plane = plane ;
 	}
 
 	public Vector getNormal(Point3D p) {
