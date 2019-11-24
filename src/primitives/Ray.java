a00d1d666e43f498dcebd4882ff009ee15082045
asafJct <44846049+asafJct@users.noreply.github.com>
2019-11-24 18:52:53 +0200

    Update Ray.java

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,23 +1,27 @@
 package primitives;
 
 public class Ray {
+	
 	private Vector vec;
-	private Point3D p;
+	
+	private Vector direction;
 
-	public Ray(Vector vec, Point3D p) {
-		
-		this.vec = vec.Normal();
-		this.p = p;
+	public Ray(Vector _vec, Vector _direction) {
+		vec = _vec;
+		direction =_directon;
 	}
-
+	
+//  #region getters
 	public Vector getVector() {
 		return vec;
 	}
 
-	public Point3D getPoint3D() {
-		return p;
+	public Vector getDirection() {
+		return direction;
 	}
-
+//  #endRegion.
+	
+	
 	@Override
 	public boolean equals(Object other) {
 		if (p.equals(other) == true) {
@@ -27,9 +31,10 @@ public class Ray {
 		}
 		return false;
 	}
-
+	
+//
 	@Override
 	public String toString() {
-		return "Ray [vec=" + vec.toString() + ", p=" + p.toString() + "]";
+		return 
 	}
 }
