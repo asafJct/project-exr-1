7af8cd552e91bec588427751c61eba9df0d7ed09
asafJct <44846049+asafJct@users.noreply.github.com>
2019-11-24 18:26:18 +0200

    Update Triangle.java

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -9,12 +9,12 @@ public class Triangle extends Polygon {
 	Point3D p1;
 	Point3D p2;
 	Point3D p3;
-
+//c'ctor.
 	public Triangle(Point _p1 ,Point _p2, Point _p3) {
-		//Initialize a list of points .
 		p1 = _p1;
 		p2 = _p2;
 		p3 = _p3;
+		//Initialize a list of points .
 		List<Point3D> _points =Lists.newArrayList(_p1, _p2,_p3);
 		super(_points);
 	}
